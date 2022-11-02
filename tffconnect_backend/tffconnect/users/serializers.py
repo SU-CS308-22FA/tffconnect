3606f03689d35b2cc96e3ab5544d1526814d00ad
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-11-02 10:32:14 +0300

    Create the User serializer

---
+++ tffconnect_backend/tffconnect/users/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+
+from . import models
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = models.User
+        fields = ["id", "username", "email", "first_name", "last_name", "password"]
+
+    def create(self, validated_data):
+        user = models.User.objects.create_user(**validated_data)
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get("username", instance.username)
+        instance.email = validated_data.get("email", instance.email)
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.save()
+        return instance
