ce752dd6307a9ab3416f7702aa718971e19d10d7
dcmetiner <92755437+dcmetiner@users.noreply.github.com>
2022-11-02 23:09:17 +0300

    Implement mock feed view and start profile view

--- tffconnect_frontend/tffconnect/src/App.js
+++ tffconnect_frontend/tffconnect/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-d
 import SignInSide from './user/SignInSide';
 import SignUp from './user/SignUp';
 import Home from './components/Home';
+import Feed from './components/Feed';
+import Settings from './user/Settings';
 
 function App() {
   return(
@@ -11,6 +13,8 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route exact path="/login" element={<SignInSide />} />
         <Route exact path="/signup" element={<SignUp />} />
+        <Route exact path="/feed" element={<Feed />} />
+        <Route exact path="/profile" element={<Settings />} />
       </Routes>
     </Router>
   );
---
+++ tffconnect_frontend/tffconnect/src/components/Feed.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+class Feed extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Successfully Logged In!</h1>
+                <br />
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        {/* Endpoint to route to Profile component */}
+                        <Link to="/profile">Profile</Link>
+                    </li>
+                </ul>
+            </div>
+        );
+    }
+};
+
+export default Feed;
