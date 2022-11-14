73162ce7811bab1e84142d12df41a1fd3ed148ad
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-11-14 17:28:57 +0300

    Update django production settings

--- tffconnect_backend/tffconnect/settings/production.py
+++ tffconnect_backend/tffconnect/settings/production.py
@@ -8,7 +8,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["tffconnect.com"]
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -16,8 +16,8 @@ ALLOWED_HOSTS = ["*"]
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "HOST": "tffconnect-db-instance-1.cjpegjn0pehu.eu-west-1.rds.amazonaws.com",
         "NAME": "tffconnect",
+        "HOST": os.environ.get("DB_HOST"),
         "USER": os.environ.get('DB_USER'),
         "PASSWORD": os.environ.get('DB_PASSWORD'),
         "PORT": 5432,
