3f640eec47b868ec3a7d1cfffa83f7515372b481
dcmetiner <92755437+dcmetiner@users.noreply.github.com>
2022-11-20 23:54:13 +0300

    Fix remaining linter errors

--- tffconnect_backend/tffconnect/news/views.py
+++ tffconnect_backend/tffconnect/news/views.py
@@ -1,12 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import News
 
 def index(request):
-    model = News
     return HttpResponse("Hello, world. You're at the news index.")
-
-    #def get_queryset(self):
-        #return HttpResponse("Hello, world. You're at the news index.")
-        #return News.objects.all()
