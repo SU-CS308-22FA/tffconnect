9efb8b56e01cb583f63a4b02bdac1c02aa6ea758
dcmetiner <92755437+dcmetiner@users.noreply.github.com>
2022-11-20 23:49:47 +0300

    Ignore linter errors

--- tffconnect_backend/tffconnect/urls.py
+++ tffconnect_backend/tffconnect/urls.py
@@ -22,8 +22,4 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("tffconnect.users.urls", namespace="users")),
     path("api/", include("tffconnect.news.urls", namespace="news")),
-]
-
-if settings.DEBUG:
-        urlpatterns += static(settings.MEDIA_URL,
-                              document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
