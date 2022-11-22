956b6a258f3b341e62fba782168c37f7552b88b2
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-11-22 08:47:37 +0300

    Use email as usernames

--- tffconnect_backend/tffconnect/users/serializers.py
+++ tffconnect_backend/tffconnect/users/serializers.py
@@ -8,7 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = models.User
-        fields = ["id", "username", "email", "first_name", "last_name", "password"]
+        fields = ["id", "username", "first_name", "last_name", "password"]
 
     def create(self, validated_data):
         user = models.User.objects.create_user(**validated_data)
@@ -16,7 +16,6 @@ class UserSerializer(serializers.ModelSerializer):
 
     def update(self, instance, validated_data):
         instance.username = validated_data.get("username", instance.username)
-        instance.email = validated_data.get("email", instance.email)
         instance.first_name = validated_data.get("first_name", instance.first_name)
         instance.last_name = validated_data.get("last_name", instance.last_name)
         instance.save()
