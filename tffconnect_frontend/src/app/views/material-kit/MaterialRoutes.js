8a4a792d428b7aea0d0866117fdec2f6d7b87a00
dcmetiner <92755437+dcmetiner@users.noreply.github.com>
2022-11-22 20:10:42 +0300

    Organize navigation bar

--- tffconnect_frontend/src/app/views/material-kit/MaterialRoutes.js
+++ tffconnect_frontend/src/app/views/material-kit/MaterialRoutes.js
@@ -15,6 +15,7 @@ const AppDialog = Loadable(lazy(() => import('./dialog/AppDialog')));
 const AppSnackbar = Loadable(lazy(() => import('./snackbar/AppSnackbar')));
 const AppAutoComplete = Loadable(lazy(() => import('./auto-complete/AppAutoComplete')));
 const AppExpansionPanel = Loadable(lazy(() => import('./expansion-panel/AppExpansionPanel')));
+const AddProject = Loadable(lazy(() => import('../AddProject')));
 
 const materialRoutes = [
   {
@@ -72,6 +73,10 @@ const materialRoutes = [
   {
     path: '/material/snackbar',
     element: <AppSnackbar />,
+  },
+  {
+    path: '/material/addproject',
+    element: <AddProject />,
   },
 ];
 
