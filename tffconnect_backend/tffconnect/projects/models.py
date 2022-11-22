9db51a4be45f5be25669879aadedd694f17d188e
sinanyilmaz1 <sinanyilmaz@sabanciuniv.edu>
2022-11-22 21:04:25 +0300

    Erase unnecesarry comments

--- tffconnect_backend/tffconnect/projects/models.py
+++ tffconnect_backend/tffconnect/projects/models.py
@@ -4,13 +4,13 @@ from tffconnect.users.models import User
 
 # Create your models here.
 class Project(models.Model):
-    name = models.CharField(max_length=255)  # should we make this field lenght bit larger?
+    name = models.CharField(max_length=255)  
     is_finished = models.BooleanField()
     proposal_date = models.DateTimeField(auto_now_add=True)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    description = models.CharField(max_length=255)  # should we make this field lenght bit larger?
-    location = models.CharField(max_length=255)  # should we make this field lenght bit larger?
+    description = models.CharField(max_length=255)  
+    location = models.CharField(max_length=255)  
     budget = models.PositiveIntegerField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     is_confirmed_by_tff = models.BooleanField()
