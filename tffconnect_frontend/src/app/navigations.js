8fdd6cf7e9300ae9b93518cbb8cb1201e6b460b4
dcmetiner <92755437+dcmetiner@users.noreply.github.com>
2022-11-22 20:13:41 +0300

    Add projects route to navigation bar

--- tffconnect_frontend/src/app/navigations.js
+++ tffconnect_frontend/src/app/navigations.js
@@ -1,4 +1,5 @@
 export const navigations = [
+
   { name: 'Ana Sayfa', path: '/dashboard/default', icon: 'home' },
   { label: 'TFF GÜNCEL', type: 'label' },
   {
@@ -7,7 +8,7 @@ export const navigations = [
     children: [
       { name: 'Lig Durumu', iconText: 'LD', path: "" },
       { name: 'Hakemler', iconText: 'H', path: "" },
-      { name: 'Projeler', iconText: 'P', path: "" },
+      { name: 'Projeler', iconText: 'P', path: '/material/addproject' },
     ],
   },
   { label: 'OTURUM AÇ', type: 'label' },
@@ -17,41 +18,6 @@ export const navigations = [
     children: [
       { name: 'Oturum Aç', iconText: 'SI', path: '/session/signin' },
       { name: 'Kayıt Ol', iconText: 'SU', path: '/session/signup' },
-      //{ name: 'Forgot Password', iconText: 'FP', path: '/session/forgot-password' },
-      //{ name: 'Error', iconText: '404', path: '/session/404' },
     ],
-  },
-  { label: 'Components', type: 'label' },
-  {
-    name: 'Components',
-    icon: 'favorite',
-    badge: { value: '30+', color: 'secondary' },
-    children: [
-      { name: 'Auto Complete', path: '/material/autocomplete', iconText: 'A' },
-      { name: 'Buttons', path: '/material/buttons', iconText: 'B' },
-      { name: 'Checkbox', path: '/material/checkbox', iconText: 'C' },
-      { name: 'Dialog', path: '/material/dialog', iconText: 'D' },
-      { name: 'Expansion Panel', path: '/material/expansion-panel', iconText: 'E' },
-      { name: 'Form', path: '/material/form', iconText: 'F' },
-      { name: 'Icons', path: '/material/icons', iconText: 'I' },
-      { name: 'Menu', path: '/material/menu', iconText: 'M' },
-      { name: 'Progress', path: '/material/progress', iconText: 'P' },
-      { name: 'Radio', path: '/material/radio', iconText: 'R' },
-      { name: 'Switch', path: '/material/switch', iconText: 'S' },
-      { name: 'Slider', path: '/material/slider', iconText: 'S' },
-      { name: 'Snackbar', path: '/material/snackbar', iconText: 'S' },
-      { name: 'Table', path: '/material/table', iconText: 'T' },
-    ],
-  },
-  {
-    name: 'Charts',
-    icon: 'trending_up',
-    children: [{ name: 'Echarts', path: '/charts/echarts', iconText: 'E' }],
-  },
-  {
-    name: 'Documentation',
-    icon: 'launch',
-    type: 'extLink',
-    path: 'http://demos.ui-lib.com/matx-react-doc/',
   },
 ];
