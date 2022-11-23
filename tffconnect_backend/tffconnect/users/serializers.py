30d90b6f49cca514c6690e7eef1cbdf1175f449d
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-11-23 13:09:38 +0300

    Add google-signup-login backend endpoint

--- tffconnect_backend/tffconnect/users/serializers.py
+++ tffconnect_backend/tffconnect/users/serializers.py
@@ -1,6 +1,9 @@
 from rest_framework import serializers
+from google.oauth2 import id_token as google_id_token
+from google.auth.transport import requests
+from google.auth.exceptions import GoogleAuthError
 
-from . import models
+from . import models, constants
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -20,3 +23,26 @@ class UserSerializer(serializers.ModelSerializer):
         instance.last_name = validated_data.get("last_name", instance.last_name)
         instance.save()
         return instance
+
+
+class GoogleSignupLoginSerializer(serializers.Serializer):
+    credential = serializers.CharField()
+
+    def validate(self, data):
+        credential = data["credential"]
+        try:
+            id_info = google_id_token.verify_oauth2_token(
+                credential, requests.Request(), constants.FRONTEND_GOOGLE_CLIENT_ID
+            )
+        except (ValueError, GoogleAuthError) as e:
+            raise serializers.ValidationError({"credential": f"Invalid token: {e}"})
+
+        user, _ = models.User.objects.get_or_create(
+            username=id_info["email"],
+            defaults={
+                "first_name": id_info["given_name"],
+                "last_name": id_info["family_name"],
+            },
+        )
+        data["user"] = user
+        return data
--- tffconnect_backend/tffconnect/users/views.py
+++ tffconnect_backend/tffconnect/users/views.py
@@ -1,5 +1,9 @@
+from rest_framework.views import APIView
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.authtoken.models import Token
+from django.http import HttpResponseRedirect
+from django.conf import settings
 
 from . import models, serializers
 
@@ -16,3 +20,15 @@ class MeView(RetrieveUpdateDestroyAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class GoogleSignupLoginView(APIView):
+    serializer_class = serializers.GoogleSignupLoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data["user"]
+        token, _ = Token.objects.get_or_create(user=user)
+        return HttpResponseRedirect(f"{settings.FRONTEND_URL}/?token={token.key}")
