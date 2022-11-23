6222191c7d293be0759df90bb058e430bd33f09a
sinanyilmaz1 <sinanyilmaz@sabanciuniv.edu>
2022-11-23 16:25:41 +0300

    Import allow any library

--- tffconnect_backend/tffconnect/projects/views.py
+++ tffconnect_backend/tffconnect/projects/views.py
@@ -1,5 +1,5 @@
 from rest_framework.generics import ListCreateAPIView
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 
 from .models import Project
 from .serializers import ProjectSerializer
