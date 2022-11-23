a6f9f456ed37df095da59c30d4fef554e88fe4eb
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-11-23 13:12:46 +0300

    Wrap app with GoogleOAuthProvider

--- tffconnect_frontend/src/app/App.jsx
+++ tffconnect_frontend/src/app/App.jsx
@@ -6,18 +6,21 @@ import { AuthProvider } from './contexts/JWTAuthContext';
 import { SettingsProvider } from './contexts/SettingsContext';
 import { Store } from './redux/Store';
 import routes from './routes';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = () => {
   const content = useRoutes(routes);
 
   return (
-    <Provider store={Store}>
-      <SettingsProvider>
-        <MatxTheme>
-          <AuthProvider>{content}</AuthProvider>
-        </MatxTheme>
-      </SettingsProvider>
-    </Provider>
+    <GoogleOAuthProvider clientId="572073356513-hg6p2ohk4s3ktohnnd6moi9sfgfmm73d.apps.googleusercontent.com">
+      <Provider store={Store}>
+        <SettingsProvider>
+          <MatxTheme>
+            <AuthProvider>{content}</AuthProvider>
+          </MatxTheme>
+        </SettingsProvider>
+      </Provider>
+    </GoogleOAuthProvider>
   );
 };
 
