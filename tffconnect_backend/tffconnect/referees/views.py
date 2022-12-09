8db06f03bd6777ced528cb08584bbc442fe0e425
dcmetiner <92755437+dcmetiner@users.noreply.github.com>
2022-12-09 14:29:35 +0300

    fix linter errors-2

--- tffconnect_backend/tffconnect/referees/views.py
+++ tffconnect_backend/tffconnect/referees/views.py
@@ -5,6 +5,6 @@ from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class RefereesList(generics.ListCreateAPIView):
-     queryset = Referees.objects.all()
-     serializer_class = RefereesSerializer
-     permission_classes = [IsAuthenticatedOrReadOnly]
+    queryset = Referees.objects.all()
+    serializer_class = RefereesSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
--- tffconnect_backend/tffconnect/settings/base.py
+++ tffconnect_backend/tffconnect/settings/base.py
@@ -51,7 +51,7 @@ ROOT_URLCONF = "tffconnect.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [], 
+        "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
