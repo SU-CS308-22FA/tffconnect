008eb16e289db44a2ff92fbaf9d8c239364004ad
sinanyilmaz1 <sinanyilmaz@sabanciuniv.edu>
2022-12-12 16:58:02 +0300

    Fix flake8 linter errors

--- tffconnect_backend/tffconnect/projects/models.py
+++ tffconnect_backend/tffconnect/projects/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from tffconnect.users.models import User
 
+
 class Project(models.Model):
-    name = models.CharField(max_length=255)  
+    name = models.CharField(max_length=255)
     is_finished = models.BooleanField()
     proposal_date = models.DateTimeField(auto_now_add=True)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     description = models.CharField(max_length=255)  
-    location = models.CharField(max_length=255)  
+    location = models.CharField(max_length=255)
     budget = models.PositiveIntegerField()
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     is_confirmed_by_tff = models.BooleanField()
