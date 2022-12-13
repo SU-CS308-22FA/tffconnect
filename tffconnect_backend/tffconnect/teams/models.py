b98b7e371c3b87e4bfb6144ef61d14c1262e18d0
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-12-13 17:15:37 +0300

    Add str method to team

--- tffconnect_backend/tffconnect/teams/models.py
+++ tffconnect_backend/tffconnect/teams/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Team(models.Model):
     name = models.CharField(max_length=100)
     league = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
