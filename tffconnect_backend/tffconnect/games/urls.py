b238488b7ca891cf8ad26afdc98208c6defafdf6
Alp Helvaci <57733277+alphelvaci@users.noreply.github.com>
2022-12-26 13:01:31 +0300

    Create a game comments endpoint

--- tffconnect_backend/tffconnect/games/urls.py
+++ tffconnect_backend/tffconnect/games/urls.py
@@ -7,4 +7,5 @@ app_name = "games"
 urlpatterns = [
     path("games/", views.AddGames.as_view()),
     path("games/<int:id>/", views.ModifyGames.as_view()),
+    path("games/<int:id>/comments/", views.GameCommentListCreateAPIView.as_view()),
 ]
