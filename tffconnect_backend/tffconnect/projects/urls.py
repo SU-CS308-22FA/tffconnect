cbeb0187927051d0f4971f29b4baca3ec9b00284
sinanyilmaz1 <sinanyilmaz@sabanciuniv.edu>
2022-12-27 18:54:11 +0300

    Update URL

--- tffconnect_backend/tffconnect/projects/urls.py
+++ tffconnect_backend/tffconnect/projects/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("projects/", views.ProjectListCreateView.as_view()),
     path("projects/edit/<int:pk>/", views.ProjectRetrieveUpdateDestroyView.as_view()),
     path("projects/comments/", views.ProjectCommentListCreateView.as_view()),
-    path("projects/comments/edit/<int:pk>/", views.ProjectCommentRetrieveUpdateDestroyView.as_view()),
+    path("projects/comments/<int:pk>/", views.ProjectCommentRetrieveUpdateDestroyView.as_view()),
 ]
