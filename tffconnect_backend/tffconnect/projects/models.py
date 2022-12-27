692410fdcb474c5d337015a83b94a51f77752e52
sinanyilmaz1 <sinanyilmaz@sabanciuniv.edu>
2022-12-27 18:23:52 +0300

    Initialize Project Comment model

--- tffconnect_backend/tffconnect/projects/models.py
+++ tffconnect_backend/tffconnect/projects/models.py
@@ -14,3 +14,17 @@ class Project(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     is_confirmed_by_tff = models.BooleanField()
     confirmation_datetime = models.DateTimeField(auto_now_add=True)
+
+class ProjectComment(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE) # char field?
+    text_body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved = True
+        self.save()
+    
+    def __str__(self):
+        return self.text_body
