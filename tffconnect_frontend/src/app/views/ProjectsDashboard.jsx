2e528921ae8e55a15f836027cbcab76d0cd0982b
sinanyilmaz1 <sinanyilmaz@sabanciuniv.edu>
2022-12-30 14:16:57 +0300

    Implemented a comment view

--- tffconnect_frontend/src/app/views/ProjectsDashboard.jsx
+++ tffconnect_frontend/src/app/views/ProjectsDashboard.jsx
@@ -16,93 +16,172 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import { Grid } from '../../../node_modules/@mui/material/index';
+import { Container, Grid } from '../../../node_modules/@mui/material/index';
 import { Fragment } from 'react';
+import { API_URL } from 'app/constants';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import CommentIcon from '@mui/icons-material/Comment';
+import { async } from '../../../node_modules/regenerator-runtime/runtime';
 
-const ExpandMore = styled((props) => {
-    const { expand, ...other } = props;
-    return <IconButton {...other} />;
-  })(({ theme, expand }) => ({
-    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-    marginLeft: 'auto',
-    transition: theme.transitions.create('transform', {
-      duration: theme.transitions.duration.shortest,
-    }),
-}));
 
 const ContentBox = styled('div')(({ theme }) => ({
     margin: '30px',
     [theme.breakpoints.down('sm')]: { margin: '16px' },
 }));
 
-const bull = (
-    <Box
-      component="span"
-      sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
-    >
-        •
-    </Box>
-);
   
-
 export default function ProjectDashboard() {
-    const [expanded, setExpanded] = React.useState(false);
-    const handleExpandClick = () => {
-        setExpanded(!expanded);
+    const [isexpanded, setIsExpanded] = useState({});
+
+    let [allProjects, setResponseData] = useState([]);
+    let [allComments, setComments] = useState([]);
+
+    
+    const handleExpandClick = (projectid_index) => {
+        if (isexpanded[projectid_index] === undefined) {
+            setIsExpanded({
+                ...isexpanded,
+                [projectid_index]: false
+            });
+        }
+        else {
+            
+            setIsExpanded({
+                ...isexpanded,
+                [projectid_index]: !isexpanded[projectid_index]
+            });
+        }
+        
+        console.log(isexpanded[projectid_index]);
+
+        console.log(projectid_index);
+        console.log("hey");
+    };
+    
+    const defineIsExpanded = (projectid_index) => {
+        if (isexpanded[projectid_index] === undefined) {
+            setIsExpanded({
+                ...isexpanded,
+                [projectid_index]: false
+            });
+        }
     };
 
     const handleSettingsClick = () => {
         console.log("REPORTED");
     };
+
+    useEffect(() => {
+        getProjectItems();
+
+    }, []);
+
+    const  getProjectItems = () => {
+
+        axios.get(API_URL + '/projects/')
+            .then(response => {
+                allProjects = response.data;
+                console.log(allProjects);
+                setResponseData(allProjects);
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+        axios.get(API_URL + '/projects/comments/')
+            .then(response2 => {
+                allComments = response2.data;
+                console.log(allComments);
+                setComments(allComments);
+            })
+            .catch(err2 => {
+                console.log(err2);
+            })
+    };
+
     return (
         <Fragment>
             <ContentBox className ="ProjectDashboard">
-                <Grid container spacing={2}>
+                <Grid container spacing={-2}>
                     <Grid item xs={12} sm={6} md={4}>
 
-                        <Card sx={{ minWidth: 275 }}>
-                            <CardHeader title = "Proje 1" subheader = "BAŞLAMA TARİHi" 
-                                action = {
-                                    <IconButton aria-label="settings" onClick = {()=> handleSettingsClick()}>
-                                        <MoreVertIcon />
-                                    </IconButton>
-                                }  
-                            />
-                            <CardContent>
-                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                                    PROJE bütçesi
-                                </Typography>
-                                <Typography variant="h5" component="div">
-                                    PROJE 1 DESCRIPTION
-                                </Typography>
-                            </CardContent>
-                            <CardActions disableSpacing>
-                                <IconButton aria-label="add to favorites">
-                                    <FavoriteIcon />
-                                </IconButton>
-                                <IconButton aria-label="share">
-                                    <ShareIcon />
-                                </IconButton>
-                                <ExpandMore
-                                expand={expanded}
-                                onClick={() => handleExpandClick()}
-                                aria-expanded={expanded}
-                                aria-label="show more"
-                                >
-                                    <ExpandMoreIcon />
-                                </ExpandMore>
-                            </CardActions>
-                            <Collapse in={expanded} timeout="auto" unmountOnExit>
-                                <CardContent>
-                                <Typography paragraph>
-                                    Method:
-                                </Typography>
-                                <Typography paragraph>
-                                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
-                                    aside for 10 minutes.
-                                </Typography>
-                                </CardContent>
-                            </Collapse>
+                        {(() => {
+                            let len = allProjects.length;
+
+                            if (len === 0) {
+                                return (
+                                <Card sx={{ minWidth: 275, px: 3, py: 2, mb: 3  }}>
+                                    <CardContent>
+                                        <Typography variant="h5" component="div">
+                                                PROJE BULUNAMADI
+                                        </Typography>
+                                    </CardContent>
+                                </Card>
+                                );
+                            }
+                            else{
+                                let list = [];
+                                let projectName,projectStartDate, projectBudget, projectDescription;
+                                for (let i = 0; i<len ; i++){
+                                    defineIsExpanded(i);
+                                    projectName = allProjects[i].name;
+                                    projectStartDate = allProjects[i].start_date;
+                                    projectBudget = allProjects[i].budget;
+                                    projectDescription = allProjects[i].description;
+                                    list.push(
+                                        <Card expanded={isexpanded[i]} sx={{ minWidth: 275, px: 3, py: 2, mb: 3 }}>
+                                            <CardHeader title={`${projectName}`} subheader = {`Başlangiç tarihi: ${projectStartDate}`}
+                                                action = {
+                                                    <IconButton aria-label="settings" onClick = {()=> handleSettingsClick()}>
+                                                        <MoreVertIcon />
+                                                    </IconButton>
+                                                }  
+                                            />
+                                            <CardContent>
+                                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                    Proje Bütçesi: {`${projectBudget}`}
+                                                </Typography>
+                                                <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                                    Proje Açıklaması: {`${projectDescription}`}
+                                                </Typography>
+                                            </CardContent>
+                                            <CardActions disableSpacing>
+                                                <IconButton aria-label="add to favorites">
+                                                    <FavoriteIcon />
+                                                </IconButton>
+                                                <IconButton>
+                                                    <CommentIcon onClick = { ()=> handleExpandClick(i) }/>
+                                                </IconButton>
+                                            </CardActions>
+
+                                            <Collapse in={isexpanded[i] === true} timeout="auto" unmountOnExit>
+                                                <CardContent>
+                                                <Typography paragraph>
+                                                    Method:
+                                                </Typography>
+                                                <Typography paragraph>
+                                                    Heat 1/2 cup of the broth in a pot until simmering, add saffron and set
+                                                    aside for 10 minutes.
+                                                </Typography>
+                                                </CardContent>
+                                            </Collapse>
+                                        </Card>
+                                    );
+                                }
+                                return list;
+                            }
+                        })()}
+                    </Grid>
+                    <Grid item xs={6} sm={3} md={0.5}>
+                        <Container>
+                        
+                        </Container>
+                    </Grid>
+                    Burda
+                    <Grid item xs={12} sm={6} md={4}>
+                        <Card sx={{ minWidth: 275, px: 3, py: 2, mb: 3 }}>
+
                         </Card>
                     </Grid>
                 </Grid>
