feafd8c319b44630bc15928fb674602a721443cf
sinanyilmaz1 <sinanyilmaz@sabanciuniv.edu>
2023-01-04 23:52:28 +0300

    Create project favorite serializer

--- tffconnect_backend/tffconnect/projects/serializers.py
+++ tffconnect_backend/tffconnect/projects/serializers.py
@@ -74,3 +74,31 @@ class ProjectCommentSerializer(serializers.ModelSerializer):
         instance.is_approved = validated_data.get('is_approved', instance.is_approved)
         instance.save()
         return instance
+
+class ProjectFavoriteSerializer(serializers.ModelSerializer):
+    
+        class Meta:
+            model = models.ProjectFavorite
+            fields = (
+                'id',
+                'project',
+                'liked_by',
+                'date_liked',
+                'is_liked'
+            )
+            
+        def create(self, validated_data):
+            project_favorite = models.ProjectFavorite.objects.create(**validated_data)
+            return project_favorite
+
+        def delete(self, validated_data):
+            project_favorite = models.ProjectFavorite.objects.delete(**validated_data)
+            return project_favorite
+
+        def update(self, instance, validated_data):
+            instance.project = validated_data.get('project', instance.project)
+            instance.liked_by = validated_data.get('liked_by', instance.liked_by)
+            instance.date_liked = validated_data.get('date_liked', instance.date_liked)
+            instance.is_liked = validated_data.get('is_liked', instance.is_liked)
+            instance.save()
+            return instance
