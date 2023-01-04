d5f50a49bae55a0812d9dd204e61e1af1cd064bb
ilginarat <ilginelif@sabanciuniv.edu>
2023-01-04 14:25:19 +0300

    backend referees app

--- tffconnect_backend/tffconnect/referees/serializers.py
+++ tffconnect_backend/tffconnect/referees/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 from .models import Referees
+from . import models
 
 
 class RefereesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Referees
-        fields = ('id', 'name', 'surname', 'city', 'classification')
+        fields = (
+                'id',
+                'name',
+                'surname',
+                'city',
+                'classification'
+        )
+
+        def create (self, validated_data):
+                referee = models.Referees.objects.create(**validated_data)
+                return referee
+
+        def delete(self, validated_data):
+                referee = models.Referees.objects.delete(**validated_data)
+                return referee
+
