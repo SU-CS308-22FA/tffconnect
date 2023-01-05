714977c16ed70ccd82c108bf7ff65b35f2fb2b72
dcmetiner <92755437+dcmetiner@users.noreply.github.com>
2023-01-05 14:08:13 +0300

    Navigation bar hotfix

--- tffconnect_frontend/src/app/navigations.js
+++ tffconnect_frontend/src/app/navigations.js
@@ -7,22 +7,11 @@ export const navigations = [
     name: 'Güncel Veriler',
     icon: 'public',
     children: [
-      { name: 'Lig Durumu', path: "" },
-      { name: 'Hakemler', path: "" },
       { name: 'Hakemler-Maçlar', path: '/data/referee_games' },
       { name: 'Tüm Projeler', path: "/material/ProjectsDashboard" },
       { name: 'Proje Ekle', iconText: 'PE', path: '/material/addproject' },
       { name: 'Projelerimi Listele', iconText: 'PL', path: '/material/listproject' },
       { name: 'Profilim', iconText: 'P', path: '/material/userprofile' },
-    ],
-  },
-  { label: 'OTURUM AÇ', type: 'label' },
-  {
-    name: 'Giriş Yap',
-    icon: 'person',
-    children: [
-      { name: 'Oturum Aç', iconText: 'SI', path: '/session/signin' },
-      { name: 'Kayıt Ol', iconText: 'SU', path: '/session/signup' },
     ],
   },
 ];
